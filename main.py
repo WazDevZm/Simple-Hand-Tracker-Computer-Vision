bb0f023541eb8b2cd3d020002d017a34ea79114b
Wazingwa Mugala <wazingwamugala90@gmail.com>
2024-11-12 07:41:00 +0200

    The final project

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 ##Simple Hand Tracking App
 ##Developed by : Wazingwa Mugala
+import cvzone
+import mediapipe
+import cv2
+from cvzone.HandTrackingModule import HandDetector
 
 
+cap = cv2.VideoCapture(0)
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
+
+while True:
+    success, img = cap.read()
+    img = cv2.flip (img, 1)
+    
+    detector.findHands(img, flipType=False)
+    
+    
+    cv2.imshow("Hand-Tracker App", img)
+    cv2.waitKey(1)
+    
